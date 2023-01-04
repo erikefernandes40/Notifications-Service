c75111946eb94dee59e9d6be4779616e51fce4eb
erikefernandes40 <erikefernandes40@gmail.com>
2023-01-04 12:06:08 -0300

    test: cria testes para a entidade content

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,17 @@
+import { Content } from './content';
+
+describe('Notification Content', () => {
+  it('it should be able to create a notification content', () => {
+    const content = new Content('Voce recebeu uma solicitação de amizade!');
+
+    expect(content).toBeTruthy();
+  });
+
+  it('it should not be able to create a notification content with less than 5 caracters', () => {
+    expect(() => new Content('aaa')).toThrow('Content length error.');
+  });
+
+  it('it should not be able to create a notification content with more than 240 caracters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow('Content length error.');
+  });
+});
